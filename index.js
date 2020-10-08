b175334f1ca0b120dd66fe1ba8f9785685499d85
Justin Teo Sze Wei <justin@Justins-MacBook-Pro.local>
2020-10-08 13:50:13 +0800

    Adding additional adlib screenshot route

--- index.js
+++ index.js
@@ -4,12 +4,22 @@ const port = process.env.PORT || 3131
 const screenshot = require('./screenshot')
 
 app.get('/', (req, res) => res.status(200).json({ status: 'ok' }))
-
 app.get('/screenshot', (req, res) => {
   var url = req.query.url + "&t=" + req.query.t 
   var preview_type=req.query.preview_type
   ;(async () => {
     const buffer = await screenshot(url,preview_type)
+    res.setHeader('Content-Disposition', 'attachment; filename="screenshot.png"')
+    res.setHeader('Content-Type', 'image/png')
+    res.send(buffer)
+  })()
+})
+
+app.get('/adlibscreenshot', (req, res) => {
+  var ad_id = req.query.id
+  var url = 'https://www.facebook.com/ads/library/?id=' + ad_id 
+  ;(async () => {
+    const buffer = await screenshot(url)
     res.setHeader('Content-Disposition', 'attachment; filename="screenshot.png"')
     res.setHeader('Content-Type', 'image/png')
     res.send(buffer)
--- screenshot.js
+++ screenshot.js
@@ -1,30 +1,50 @@
 const puppeteer = require("puppeteer");
 
-module.exports = function (url,preview_type) {
+module.exports = function (url,preview_type='') {
   return new Promise((resolve, reject) => {
     (async () => {
       const browser = await puppeteer.launch({
         args: ["--no-sandbox"],
       });
       const page = await browser.newPage();
-
+      await page.setViewport({ width: 1920, height: 1590 });
       await page.goto(url, {
         waitUntil: ["networkidle0", "domcontentloaded"],
       });
 
       await new Promise((resolve) => setTimeout(resolve, 8000));
-      
+
       var padding = 75;
       var left = 0;
       var top = 0;
       var selector =
         "#fb-ad-preview > div > div > div:nth-child(1) > div > div";
-        if(preview_type == "MOBILE_FEED_STANDARD"){
-          selector = "#ad-preview-mobile-feed-standard > div";
-          padding = 20;
-          left = 340;
-        }   
+      if(preview_type == "MOBILE_FEED_STANDARD"){
+        selector = "#ad-preview-mobile-feed-standard > div";
+        padding = 20;
+        left = 340;
+      } 
+        
+      if(!preview_type){ // adlib case
+        await page.keyboard.press(String.fromCharCode(13)) //Pressing enter to clear the pop-up request error modal on facebook ad library
+        
+        const elmHandlerArray = await page.$$('div[stickto="WINDOW"]')
+        const elmHandler = elmHandlerArray[1]
+        const elmHandlerClassName = await page.evaluate(el => el.className,elmHandler)
 
+        selector = 
+          'div[class=' + '"' +elmHandlerClassName + '"] ' + ' ~ div ~div > :nth-child(3)'; 
+        var screenshotElement = await page.$(selector)
+        if(!screenshotElement){
+          selector = 'div[class=' + '"' +elmHandlerClassName + '"] ' + ' ~ div ~div ~div > :nth-child(3)';
+          screenshotElement = await page.$(selector)
+        }
+        if(!screenshotElement){
+          selector = 'div[class=' + '"' +elmHandlerClassName + '"] ' + ' ~div ~ div ~div ~div > :nth-child(3)';      
+        }
+        padding = 0
+      }
+       
       const rect = await page.evaluate((selector) => {
         const element = document.querySelector(selector);
         const { x, y, width, height } = element.getBoundingClientRect();
@@ -35,17 +55,16 @@ module.exports = function (url,preview_type) {
         throw Error(
           `Could not find element that matches selector: ${selector}.`
         );
-
-      await page.setViewport({ width: 1024, height: 800 });
+      
       const buffer = await page.screenshot({
           path: "element.png",
           type: "png",
           clip: {
             x: left !== 0 ? left : rect.left,
             y: top !== 0 ? top : rect.top,
-            width: rect.width + padding ,
+            width: rect.width + padding,
             height: rect.height + padding,
-          },
+          }
         });
     
       await browser.close();
